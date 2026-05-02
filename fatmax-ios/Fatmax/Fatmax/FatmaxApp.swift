291d3cf70336b5bcc65a6e6add1d219209334b5a
Shannon Price <shannonp5934@gmail.com>
2026-05-02 15:08:31 -0400

    adding xcode scaffold

---
+++ fatmax-ios/Fatmax/Fatmax/FatmaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FatmaxApp.swift
+//  Fatmax
+//
+//  Created by shannon price on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct FatmaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
